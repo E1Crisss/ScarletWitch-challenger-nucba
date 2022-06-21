1fb7232ed243d3ac2fb34b14bfefa4f3a58721c2
E1Crisss <cristiangau@hotmail.com>
2022-06-20 23:26:30 -0300

    cuarta subida areglando js

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const names = document.getElementById('name'),
-      email = document.getElementById('email'),
-      form = document.getElementById('formulario'),
-      errorE = document.getElementById('error');
+const names = document.getElementById("name"),
+  email = document.getElementById("email"),
+  form = document.getElementById("formulario"),
+  errorE = document.getElementById("error");
 
 const tomarValor = (e) => {
-    e.preventDefault();
-    
-  /*   if(names){
-        let dataName = names.value;
-        names.value = '';
-    }else{
-        alert('ingresar Nombre')
-        console.log('no ingreso datos');
-        names.value = '';
-    } */
-    
-    let dataName = names.value;
-    let dataEmail = email.value;
-    
-    
-    names.value = '';
-    email.value = '';
-    console.log(`Nombre: ${dataName}, Correo: ${dataEmail}`)
-}
+  e.preventDefault();
 
+  let dataName = names.value;
+  let dataEmail = email.value;
 
-form.addEventListener('submit',tomarValor);
+  names.value = "";
+  email.value = "";
+
+  console.log(`Nombre: ${dataName}, Correo: ${dataEmail}`);
+  alert("Gracias por suscribirse a Noticias Nucba");
+};
+
+form.addEventListener("submit", tomarValor);
